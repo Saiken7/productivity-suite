63a56f291e58e1b303983447ee45eca36f934a60
Kunal <saikenhere@gmail.com>
2025-09-28 19:34:09 +0530

    StickyNotesService Added

--- backend/LifeCanvas/src/main/java/com/productivity_suite/LifeCanvas/Services/StickyNotesService.java
+++ backend/LifeCanvas/src/main/java/com/productivity_suite/LifeCanvas/Services/StickyNotesService.java
@@ -1,4 +1,20 @@
 package com.productivity_suite.LifeCanvas.Services;
 
+import com.productivity_suite.LifeCanvas.Entity.StickyNotes;
+import com.productivity_suite.LifeCanvas.Repository.StickyNoteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class StickyNotesService {
+
+    @Autowired
+    private StickyNoteRepository stickyNoteRepository;
+
+    public List<StickyNotes> getAllStickyNotes(String userId){
+        return stickyNoteRepository.findByUserIdOrderByCreatedAtDesc(userId);
+    }
+
 }
