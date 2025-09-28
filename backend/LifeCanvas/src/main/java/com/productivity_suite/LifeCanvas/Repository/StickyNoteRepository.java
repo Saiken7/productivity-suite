f808b53eefe4601abdbc08026b525226e46869b8
Kunal <saikenhere@gmail.com>
2025-09-28 18:28:50 +0530

    StickyNotes Repository Added

---
+++ backend/LifeCanvas/src/main/java/com/productivity_suite/LifeCanvas/Repository/StickyNoteRepository.java
@@ -0,0 +1,10 @@
+package com.productivity_suite.LifeCanvas.Repository;
+
+import com.productivity_suite.LifeCanvas.Entity.StickyNotes;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface StickyNoteRepository extends MongoRepository<StickyNotes, String> {
+    List<StickyNotes> findByUserId(String userId);
+}
