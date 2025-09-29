83d43397e4e22ce0f961a233971af3d4b100ddd6
Kunal <saikenhere@gmail.com>
2025-09-29 19:29:55 +0530

    Added Create Note Feature

--- backend/LifeCanvas/src/main/java/com/productivity_suite/LifeCanvas/Controller/StickyNotesController.java
+++ backend/LifeCanvas/src/main/java/com/productivity_suite/LifeCanvas/Controller/StickyNotesController.java
@@ -1,11 +1,18 @@
 package com.productivity_suite.LifeCanvas.Controller;
 
 import com.productivity_suite.LifeCanvas.Entity.StickyNotes;
+import com.productivity_suite.LifeCanvas.Entity.UserEntity;
+import com.productivity_suite.LifeCanvas.Repository.UserRepository;
+import com.productivity_suite.LifeCanvas.Requests.StickyNotesDTO;
 import com.productivity_suite.LifeCanvas.Services.StickyNotesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.CurrentSecurityContext;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,14 +23,36 @@ public class StickyNotesController {
     @Autowired
     private StickyNotesService stickyNotesService;
 
+    @Autowired
+    private UserRepository userRepository;
+
     @GetMapping("/api/service/v2/get-notes")
-    public ResponseEntity<List<StickyNotes>> getAllNotes(String userId){
+    public ResponseEntity<List<StickyNotes>> getAllNotes(@CurrentSecurityContext(expression = "authentication?.name")
+                                                             String email){
+        UserEntity user = userRepository.findByEmail(email)
+                .orElseThrow(()-> new UsernameNotFoundException("User not Found"));
+
+        String userId = user.getUserId();
+
         if(userId == null){
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
-
         List<StickyNotes> notes = stickyNotesService.getAllStickyNotes(userId);
         return ResponseEntity.ok(notes);
+    }
+
+    @PostMapping("/api/service/v2/create-note")
+    public ResponseEntity<?> createNote(@CurrentSecurityContext(expression = "authentication?.name") String email,
+                                        @RequestBody StickyNotesDTO notesDTO){
+        UserEntity user = userRepository.findByEmail(email)
+                .orElseThrow(()->new UsernameNotFoundException("Email not found"));
+
+        String userId = user.getUserId();
+        if(userId != null){
+            stickyNotesService.createNewNote(userId, notesDTO);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        }
+        return new ResponseEntity<>("Something Went Wrong",HttpStatus.BAD_REQUEST);
     }
 
 
