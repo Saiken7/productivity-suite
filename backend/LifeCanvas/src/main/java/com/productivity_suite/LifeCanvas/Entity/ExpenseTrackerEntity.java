b000ed548e6dc9f1ffe6441e5db955b0000183ad
Kunal <saikenhere@gmail.com>
2025-10-03 12:57:02 +0530

    Expense Tracker Entity

--- backend/LifeCanvas/src/main/java/com/productivity_suite/LifeCanvas/Entity/ExpenseTrackerEntity.java
+++ backend/LifeCanvas/src/main/java/com/productivity_suite/LifeCanvas/Entity/ExpenseTrackerEntity.java
@@ -1,4 +1,54 @@
 package com.productivity_suite.LifeCanvas.Entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "expense_tracker", indexes = {
+        @Index(name = "idx_user_id" , columnList = "user_id"),
+        @Index(name = "idx_user_transaction_date", columnList = "user_id, transaction_date")
+})
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ExpenseTrackerEntity {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false, foreignKey = @ForeignKey(name = "fk_expense_user"))
+    @JsonIgnore
+    @NotNull(message = "User is Required")
+    private UserEntity user;
+
+    @Column(name = "amount", nullable = false, precision = 10, scale = 2)
+    @NotBlank(message = "Amount should never be blank!!")
+    @Positive(message = "Amount should always be positive")
+    private BigDecimal amount;
+
+    @Size(min = 1, max = 30, message = "Description cannot exceed 30 charecters")
+    @NotBlank(message = "There should be Description for an Expense!")
+    private String description;
+
+    @Column(name = "transaction_date", nullable = false)
+    @NotNull(message = "Transaction Date is Compulsory")
+    private LocalDate transactionDate;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
 }
