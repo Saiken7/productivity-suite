781778a2c7c01e23c78a66236a66ce5f542c0df2
Kunal <saikenhere@gmail.com>
2025-10-05 16:41:09 +0530

    Partially Completed - Delete Endpoint Remains

--- backend/LifeCanvas/src/main/java/com/productivity_suite/LifeCanvas/Controller/ExpenseTrackerController.java
+++ backend/LifeCanvas/src/main/java/com/productivity_suite/LifeCanvas/Controller/ExpenseTrackerController.java
@@ -4,19 +4,23 @@ package com.productivity_suite.LifeCanvas.Controller;
 import com.productivity_suite.LifeCanvas.Entity.ExpenseTrackerEntity;
 import com.productivity_suite.LifeCanvas.Entity.UserEntity;
 import com.productivity_suite.LifeCanvas.Repository.UserRepository;
+import com.productivity_suite.LifeCanvas.Requests.ExpenseRangeDTO;
 import com.productivity_suite.LifeCanvas.Requests.ExpenseTrackerDTO;
 import com.productivity_suite.LifeCanvas.Services.ExpenseTrackerService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.graphql.GraphQlProperties;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.CurrentSecurityContext;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -29,19 +33,49 @@ public class ExpenseTrackerController {
     private ExpenseTrackerService expenseTrackerService;
 
     @GetMapping("/api/service/v2/fetch-expenses")
-    public ResponseEntity<?> getAllExpenses(@Valid @RequestBody ExpenseTrackerDTO request,
-                                            @CurrentSecurityContext(expression = "authentication?.name") String email){
+    public ResponseEntity<?> getAllExpenses(@CurrentSecurityContext(expression = "authentication?.name") String email){
         UserEntity user = userRepository.findByEmail(email)
                 .orElseThrow(()-> new UsernameNotFoundException("User Not Found"));
 
         String userId = user.getUserId();
 
         if(userId != null){
-            List<ExpenseTrackerEntity> expenses  = expenseTrackerService.getUserExpenses(userId);
+            List<ExpenseTrackerEntity> expenses  = expenseTrackerService.getUserExpense(userId);
             return new ResponseEntity<>(expenses, HttpStatus.OK);
         }
 
         return new ResponseEntity<>("Something Went Wrong",HttpStatus.BAD_REQUEST);
     }
+
+    @GetMapping("/api/service/v2/get-expense-range")
+    public ResponseEntity<?> getExpenseFromARange(@CurrentSecurityContext(expression = "authentication?.name") String email,
+                                                  @RequestBody ExpenseRangeDTO request){
+        UserEntity user = userRepository.findByEmail(email)
+                .orElseThrow(()-> new UsernameNotFoundException("User not Found"));
+
+        String userId = user.getUserId();
+
+        if(userId != null){
+            List<ExpenseTrackerEntity> expensesRange = expenseTrackerService.getExpenseFromRange(userId, request.getStart(), request.getEnd());
+            return new ResponseEntity<>(expensesRange, HttpStatus.OK);
+        }
+        return new ResponseEntity<>("Something Went Wrong" , HttpStatus.BAD_REQUEST);
+    }
+
+    @PostMapping("/api/service/v2/add-expense")
+    public ResponseEntity<?> addExpense(@Valid @RequestBody ExpenseTrackerDTO request,
+                                        @CurrentSecurityContext(expression = "authentication?.name")String email){
+        UserEntity user = userRepository.findByEmail(email)
+                .orElseThrow(()-> new UsernameNotFoundException("User not Found"));
+
+        if(user != null){
+            expenseTrackerService.addNewExpense(email, request);
+            return new ResponseEntity<>("Expense Added", HttpStatus.CREATED);
+        }
+
+        return new ResponseEntity<>("Something went wrong", HttpStatus.BAD_REQUEST);
+    }
+
+
 
 }
