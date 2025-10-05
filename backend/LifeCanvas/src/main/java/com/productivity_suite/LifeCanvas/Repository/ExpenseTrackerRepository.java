aabefc7c5d59210ff367081db43f1c03cacaa7d6
Kunal <saikenhere@gmail.com>
2025-10-05 17:45:39 +0530

    Version-1 Complete

--- backend/LifeCanvas/src/main/java/com/productivity_suite/LifeCanvas/Repository/ExpenseTrackerRepository.java
+++ backend/LifeCanvas/src/main/java/com/productivity_suite/LifeCanvas/Repository/ExpenseTrackerRepository.java
@@ -1,6 +1,7 @@
 package com.productivity_suite.LifeCanvas.Repository;
 
 import com.productivity_suite.LifeCanvas.Entity.ExpenseTrackerEntity;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,8 +10,15 @@ import java.util.List;
 
 @Repository
 public interface ExpenseTrackerRepository extends JpaRepository<ExpenseTrackerEntity, Long> {
+
     List<ExpenseTrackerEntity> findByUser_UserId(String userId);
 
     List<ExpenseTrackerEntity> findByUser_UserIdAndTransactionDateBetween(String userId, LocalDate start, LocalDate end);
 
+    @Transactional
+    void deleteByExpenseIdAndUser_UserId(String expenseId, String userId);
+
+    boolean existsByExpenseId(String expenseId);
+
+    boolean existsByExpenseIdAndUser_UserId(String expenseId, String userId);
 }
