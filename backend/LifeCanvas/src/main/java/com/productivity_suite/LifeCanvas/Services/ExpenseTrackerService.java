a97f5844e3918683556838cb89e26285a5a29806
Kunal <saikenhere@gmail.com>
2025-10-05 17:46:02 +0530

    Verison-1 Complete

--- backend/LifeCanvas/src/main/java/com/productivity_suite/LifeCanvas/Services/ExpenseTrackerService.java
+++ backend/LifeCanvas/src/main/java/com/productivity_suite/LifeCanvas/Services/ExpenseTrackerService.java
@@ -5,12 +5,14 @@ import com.productivity_suite.LifeCanvas.Entity.UserEntity;
 import com.productivity_suite.LifeCanvas.Repository.ExpenseTrackerRepository;
 import com.productivity_suite.LifeCanvas.Repository.UserRepository;
 import com.productivity_suite.LifeCanvas.Requests.ExpenseTrackerDTO;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class ExpenseTrackerService {
@@ -25,6 +27,7 @@ public class ExpenseTrackerService {
         return expenseTrackerRepository.findByUser_UserId(userId);
     }
 
+    @Transactional
     public void addNewExpense(String email,ExpenseTrackerDTO request){
 
         UserEntity user = userRepository.findByEmail(email)
@@ -32,6 +35,7 @@ public class ExpenseTrackerService {
 
         ExpenseTrackerEntity expense = ExpenseTrackerEntity.builder()
                 .user(user)
+                .expenseId(UUID.randomUUID().toString())
                 .amount(request.getAmount())
                 .description(request.getDescription())
                 .transactionDate(request.getTransactionDate())
@@ -43,6 +47,14 @@ public class ExpenseTrackerService {
 
     public List<ExpenseTrackerEntity> getExpenseFromRange(String userId, LocalDate start, LocalDate end){
         return expenseTrackerRepository.findByUser_UserIdAndTransactionDateBetween(userId, start, end);
+    }
+
+    @Transactional
+    public void deleteExpense(String id, String userId){
+        if(!expenseTrackerRepository.existsByExpenseId(id)){
+            throw new RuntimeException("Expense Not found or you are not permitted to delete it");
+        }
+        expenseTrackerRepository.deleteByExpenseIdAndUser_UserId(id,userId);
     }
 
 }
