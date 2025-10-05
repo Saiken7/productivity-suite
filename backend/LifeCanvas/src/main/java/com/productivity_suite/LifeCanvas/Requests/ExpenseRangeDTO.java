c02986d3a87ce947d4791a022b1391a751ce45e7
Kunal <saikenhere@gmail.com>
2025-10-05 16:39:55 +0530

    ExpenseRangeDTO for Range Output

--- backend/LifeCanvas/src/main/java/com/productivity_suite/LifeCanvas/Requests/ExpenseRangeDTO.java
+++ backend/LifeCanvas/src/main/java/com/productivity_suite/LifeCanvas/Requests/ExpenseRangeDTO.java
@@ -1,12 +1,20 @@
 package com.productivity_suite.LifeCanvas.Requests;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.time.LocalDate;
 
 @Data
-public class ExpenseRange {
+public class ExpenseRangeDTO {
+
+    @NotNull(message = "Start Date is Required")
     @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate start;
+
+    @NotNull(message = "End Date is Required")
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDate end;
 }
