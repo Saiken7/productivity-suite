8002ed79e2b02835f6d669b84402ede010c263f0
Kunal <saikenhere@gmail.com>
2025-10-05 16:40:18 +0530

    Partially completed

--- backend/LifeCanvas/src/main/java/com/productivity_suite/LifeCanvas/Repository/ExpenseTrackerRepository.java
+++ backend/LifeCanvas/src/main/java/com/productivity_suite/LifeCanvas/Repository/ExpenseTrackerRepository.java
@@ -4,10 +4,13 @@ import com.productivity_suite.LifeCanvas.Entity.ExpenseTrackerEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
-public interface ExpenseTrackerRepository extends JpaRepository<ExpenseTrackerEntity, Object> {
-    List<ExpenseTrackerEntity> findByUserId(String userId);
+public interface ExpenseTrackerRepository extends JpaRepository<ExpenseTrackerEntity, Long> {
+    List<ExpenseTrackerEntity> findByUser_UserId(String userId);
+
+    List<ExpenseTrackerEntity> findByUser_UserIdAndTransactionDateBetween(String userId, LocalDate start, LocalDate end);
 
 }
