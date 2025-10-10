99b91068bc31a9944962d5795aa66467af4e6e91
Kunal <saikenhere@gmail.com>
2025-10-10 15:10:01 +0530

    ProductiveSession Entity

--- backend/LifeCanvas/src/main/java/com/productivity_suite/LifeCanvas/Entity/ProductiveSession.java
+++ backend/LifeCanvas/src/main/java/com/productivity_suite/LifeCanvas/Entity/ProductiveSession.java
@@ -1,4 +1,56 @@
 package com.productivity_suite.LifeCanvas.Entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+@Table(name = "productive_session", indexes = {
+        @Index(name = "idx_user_id", columnList = "user_id"),
+        @Index(name = "idx_session_id", columnList = "user_id, session_id")
+})
+@Data
+@Builder
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProductiveSession {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "session_id", unique = true)
+    private String sessionId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false, foreignKey = @ForeignKey(name = "fk_productive_user"))
+    @JsonIgnore
+    @NotNull(message = "User is required")
+    private UserEntity user;
+
+    @Column(name = "start_time")
+    private LocalDateTime startTime;
+
+    @Column(name = "end_time")
+    private LocalDateTime endTime;
+
+    @Column(name = "duration_time")
+    private Long durationTime;
+
+    @Column(name = "session_date")
+    private LocalDate sessionDate;
+
+    @Column(name = "week_number")
+    private Integer weekNumber;
+
+    @Column(name = "year")
+    private Integer year;
 }
