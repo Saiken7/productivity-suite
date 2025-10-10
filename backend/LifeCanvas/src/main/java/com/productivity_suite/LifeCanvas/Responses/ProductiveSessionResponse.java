bb32b709b3ca5c0e2d7a464074c4bba0f0abcab4
Kunal <saikenhere@gmail.com>
2025-10-10 15:12:38 +0530

    Response DTO

--- backend/LifeCanvas/src/main/java/com/productivity_suite/LifeCanvas/Responses/ProductiveSessionResponse.java
+++ backend/LifeCanvas/src/main/java/com/productivity_suite/LifeCanvas/Responses/ProductiveSessionResponse.java
@@ -1,4 +1,16 @@
 package com.productivity_suite.LifeCanvas.Responses;
 
+import com.productivity_suite.LifeCanvas.Entity.UserEntity;
+import lombok.Builder;
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
 public class ProductiveSessionResponse {
+    private String sessionId;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    private UserEntity user;
+    private Long durationTime;
 }
