1505ece8a73634e6e1bda546ee0dbeb6cf2b974b
Kunal <saikenhere@gmail.com>
2025-10-10 15:10:54 +0530

    Request DTO for ProductivitySession

--- backend/LifeCanvas/src/main/java/com/productivity_suite/LifeCanvas/Requests/ProductiveSessionRequest.java
+++ backend/LifeCanvas/src/main/java/com/productivity_suite/LifeCanvas/Requests/ProductiveSessionRequest.java
@@ -1,4 +1,14 @@
 package com.productivity_suite.LifeCanvas.Requests;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProductiveSessionRequest {
+    private LocalDateTime startTime;
 }
