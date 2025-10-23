ac2e3523ec5bead781252f62e4d6d49dfc17ed65
Kunal <saikenhere@gmail.com>
2025-10-23 16:14:41 +0530

    Repository Added

--- backend/LifeCanvas/src/main/java/com/productivity_suite/LifeCanvas/Repository/GoalsRepository.java
+++ backend/LifeCanvas/src/main/java/com/productivity_suite/LifeCanvas/Repository/GoalsRepository.java
@@ -1,4 +1,15 @@
 package com.productivity_suite.LifeCanvas.Repository;
 
-public class GoalsRepository {
+import com.productivity_suite.LifeCanvas.Entity.GoalsEntity;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface GoalsRepository extends MongoRepository<GoalsEntity, String> {
+    List<GoalsEntity> findByUserId(String userId);
+    boolean existsById(String id);
+    Optional<GoalsEntity>findById(String id);
 }
