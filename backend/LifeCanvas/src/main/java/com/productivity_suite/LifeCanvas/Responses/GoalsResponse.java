4a133ea4b2a7cafe636ca0f4a240c7be37e5f1b3
Kunal <saikenhere@gmail.com>
2025-10-23 16:16:04 +0530

    Response Class added

--- backend/LifeCanvas/src/main/java/com/productivity_suite/LifeCanvas/Responses/GoalsResponse.java
+++ backend/LifeCanvas/src/main/java/com/productivity_suite/LifeCanvas/Responses/GoalsResponse.java
@@ -1,4 +1,16 @@
 package com.productivity_suite.LifeCanvas.Responses;
 
+import com.productivity_suite.LifeCanvas.Entity.GoalsEntity;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
 public class GoalsResponse {
+    private String goalTitle;
+    private LocalDate startTime;
+    private LocalDate endTime;
+    private GoalsEntity.Status status;
 }
