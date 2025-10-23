ed7405249f751915c2acc8514f4b188ecc7ed44e
Kunal <saikenhere@gmail.com>
2025-10-23 16:15:11 +0530

    Request Class added

--- backend/LifeCanvas/src/main/java/com/productivity_suite/LifeCanvas/Requests/GoalRequest.java
+++ backend/LifeCanvas/src/main/java/com/productivity_suite/LifeCanvas/Requests/GoalRequest.java
@@ -1,4 +1,22 @@
 package com.productivity_suite.LifeCanvas.Requests;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import java.time.LocalDate;
+
+@Data
 public class GoalRequest {
+
+    @NotNull(message = "Title Should not be empty")
+    @NotBlank(message = "Title Should not be Blank")
+    @Size(min = 1, max = 40, message = "Title should be max 40 characters")
+    private String goalTitle;
+
+    @NotNull(message = "Please Select Date")
+    private LocalDate startTime;
+
+    @NotNull(message = "Please Select Date")
+    private LocalDate endTime;
 }
