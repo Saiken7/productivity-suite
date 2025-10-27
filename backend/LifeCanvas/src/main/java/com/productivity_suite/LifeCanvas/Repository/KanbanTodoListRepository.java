73bc872adc59cdbab94eafcc317510d01d655471
Kunal <saikenhere@gmail.com>
2025-10-27 18:47:23 +0530

    Kanban Repository

--- backend/LifeCanvas/src/main/java/com/productivity_suite/LifeCanvas/Repository/KanbanTodoListRepository.java
+++ backend/LifeCanvas/src/main/java/com/productivity_suite/LifeCanvas/Repository/KanbanTodoListRepository.java
@@ -1,4 +1,13 @@
 package com.productivity_suite.LifeCanvas.Repository;
 
-public class KanbanTodoListRepository {
+import com.productivity_suite.LifeCanvas.Entity.KanbanTodoListEntity;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface KanbanTodoListRepository extends MongoRepository<KanbanTodoListEntity, String> {
+    List<KanbanTodoListEntity> findByUserId(String userId);
+    Optional<KanbanTodoListEntity> findById(String id);
 }
