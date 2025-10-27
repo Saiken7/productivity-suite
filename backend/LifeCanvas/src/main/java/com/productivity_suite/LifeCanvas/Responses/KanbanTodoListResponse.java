c8f92d8a0b39f2f08ed351ce9d22a4a29378a378
Kunal <saikenhere@gmail.com>
2025-10-27 18:48:08 +0530

    Kanban Response

--- backend/LifeCanvas/src/main/java/com/productivity_suite/LifeCanvas/Responses/KanbanTodoListResponse.java
+++ backend/LifeCanvas/src/main/java/com/productivity_suite/LifeCanvas/Responses/KanbanTodoListResponse.java
@@ -1,4 +1,15 @@
 package com.productivity_suite.LifeCanvas.Responses;
 
+import com.productivity_suite.LifeCanvas.Entity.KanbanTodoListEntity;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class KanbanTodoListResponse {
+    private String id;
+    private String title;
+    private String description;
+    private KanbanTodoListEntity.Status status;
+
 }
