c9c2cb06eb22971deb6786d199ad79eb50c279ad
Kunal <saikenhere@gmail.com>
2025-10-27 18:47:47 +0530

    Kanban Request

--- backend/LifeCanvas/src/main/java/com/productivity_suite/LifeCanvas/Requests/KanbanTodoListRequest.java
+++ backend/LifeCanvas/src/main/java/com/productivity_suite/LifeCanvas/Requests/KanbanTodoListRequest.java
@@ -1,4 +1,17 @@
 package com.productivity_suite.LifeCanvas.Requests;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
 public class KanbanTodoListRequest {
+
+    @NotNull
+    @Size(min = 1, max = 30, message = "Title Should be of atleast 30 charecters")
+    private String title;
+
+    @NotNull
+    @Size(min = 1, max = 50, message = "Description should be of atleast 50 charecters")
+    private String description;
 }
