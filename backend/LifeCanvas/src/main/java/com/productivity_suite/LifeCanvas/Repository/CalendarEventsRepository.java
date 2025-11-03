b7b75abba88f242e5e5388c8db7fa52c30ff59c0
Kunal <saikenhere@gmail.com>
2025-11-03 11:38:17 +0530

    Calendar Event Repository Added

--- backend/LifeCanvas/src/main/java/com/productivity_suite/LifeCanvas/Repository/CalendarEventsRepository.java
+++ backend/LifeCanvas/src/main/java/com/productivity_suite/LifeCanvas/Repository/CalendarEventsRepository.java
@@ -1,4 +1,13 @@
 package com.productivity_suite.LifeCanvas.Repository;
 
-public class CalendarEventsRepository {
+import com.productivity_suite.LifeCanvas.Entity.CalendarEventEntity;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CalendarEventsRepository extends MongoRepository<CalendarEventEntity, String>{
+
+    List<CalendarEventEntity> findByUserId (String userId);
 }
