ecfb7cdbd5e24f884809f72f279fbc8cda3247b9
Kunal <saikenhere@gmail.com>
2025-11-03 11:39:55 +0530

    Calendar Response

--- backend/LifeCanvas/src/main/java/com/productivity_suite/LifeCanvas/Responses/CalendarEventResponse.java
+++ backend/LifeCanvas/src/main/java/com/productivity_suite/LifeCanvas/Responses/CalendarEventResponse.java
@@ -1,4 +1,16 @@
 package com.productivity_suite.LifeCanvas.Responses;
 
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
 public class CalendarEventResponse {
+    private String id;
+    private String title;
+    private boolean allDay;
+    private LocalDateTime start;
+    private LocalDateTime end;
 }
