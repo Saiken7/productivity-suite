4476224a2795e9931798648e6b197253a222cb30
Kunal <saikenhere@gmail.com>
2025-11-03 11:37:44 +0530

    Calendar Event Entity

--- backend/LifeCanvas/src/main/java/com/productivity_suite/LifeCanvas/Entity/CalendarEventEntity.java
+++ backend/LifeCanvas/src/main/java/com/productivity_suite/LifeCanvas/Entity/CalendarEventEntity.java
@@ -1,4 +1,28 @@
 package com.productivity_suite.LifeCanvas.Entity;
 
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.mongodb.core.mapping.Document;
+import java.time.LocalDateTime;
+
+@Data
+@Document(collection = "calendarEvents")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class CalendarEventEntity {
+
+    @Id
+    private String id;
+    private String userId;
+    private String title;
+    private boolean allDay;
+    private LocalDateTime start;
+    private LocalDateTime end;
+    @CreatedDate
+    private LocalDateTime createdAt;
 }
