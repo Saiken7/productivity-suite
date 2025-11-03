fa4da400f4a873bf02a2b3afcfc94fdc76466e29
Kunal <saikenhere@gmail.com>
2025-11-03 11:39:29 +0530

    Calendar Event Request Class

--- backend/LifeCanvas/src/main/java/com/productivity_suite/LifeCanvas/Requests/CalendarEventsRequest.java
+++ backend/LifeCanvas/src/main/java/com/productivity_suite/LifeCanvas/Requests/CalendarEventsRequest.java
@@ -1,4 +1,19 @@
 package com.productivity_suite.LifeCanvas.Requests;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
 public class CalendarEventsRequest {
+    @NotBlank(message = "Title Should not be Empty")
+    private String title;
+    @NotNull(message = "Mention the timeline")
+    private boolean allDay;
+    @NotNull(message = "Enter Start Date and Time")
+    private LocalDateTime start;
+
+    private LocalDateTime end;
 }
