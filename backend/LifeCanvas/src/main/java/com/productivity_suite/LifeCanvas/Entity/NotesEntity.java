dd17b64608c9d510454f04b56bc27f43c1039b84
Kunal <saikenhere@gmail.com>
2025-11-06 14:30:44 +0530

    Notes Entity Added

--- backend/LifeCanvas/src/main/java/com/productivity_suite/LifeCanvas/Entity/NotesEntity.java
+++ backend/LifeCanvas/src/main/java/com/productivity_suite/LifeCanvas/Entity/NotesEntity.java
@@ -1,4 +1,27 @@
 package com.productivity_suite.LifeCanvas.Entity;
 
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@Document(collection = "notes")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class NotesEntity {
+
+    @Id
+    private String id;
+
+    private String userId;
+    private String title;
+    private Map<String, Object> content;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
