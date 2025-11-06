728eaf1609674038a22bf627a0e6ed79618654a9
Kunal <saikenhere@gmail.com>
2025-11-06 14:31:55 +0530

    Notes Response Added

--- backend/LifeCanvas/src/main/java/com/productivity_suite/LifeCanvas/Responses/NotesResponse.java
+++ backend/LifeCanvas/src/main/java/com/productivity_suite/LifeCanvas/Responses/NotesResponse.java
@@ -1,4 +1,13 @@
 package com.productivity_suite.LifeCanvas.Responses;
 
+import lombok.Builder;
+import lombok.Data;
+import java.util.Map;
+
+@Data
+@Builder
 public class NotesResponse {
+    private String id;
+    private String title;
+    private Map<String, Object> content;
 }
