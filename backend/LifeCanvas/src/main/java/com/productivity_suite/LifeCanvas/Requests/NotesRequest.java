1d030b577b6962129fdc53289257ce460db4a022
Kunal <saikenhere@gmail.com>
2025-11-06 14:31:28 +0530

    Notes Request DTO added

--- backend/LifeCanvas/src/main/java/com/productivity_suite/LifeCanvas/Requests/NotesRequest.java
+++ backend/LifeCanvas/src/main/java/com/productivity_suite/LifeCanvas/Requests/NotesRequest.java
@@ -1,4 +1,19 @@
 package com.productivity_suite.LifeCanvas.Requests;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Data;
+import java.util.Map;
+
+@Builder
+@Data
 public class NotesRequest {
+
+    @NotBlank(message = "Title should not be empty")
+    private String title;
+
+    @NotNull(message = "Write Something")
+    private Map<String, Object> content;
+
 }
