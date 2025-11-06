3e84d386810cd5ce4b6dac0a6f5e6187da537eef
Kunal <saikenhere@gmail.com>
2025-11-06 14:31:07 +0530

    Notes Repository Added

--- backend/LifeCanvas/src/main/java/com/productivity_suite/LifeCanvas/Repository/NotesRepository.java
+++ backend/LifeCanvas/src/main/java/com/productivity_suite/LifeCanvas/Repository/NotesRepository.java
@@ -1,4 +1,13 @@
 package com.productivity_suite.LifeCanvas.Repository;
 
-public class NotesRepository {
+import com.productivity_suite.LifeCanvas.Entity.NotesEntity;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface NotesRepository extends MongoRepository<NotesEntity, String> {
+    List<NotesEntity> findByUserId(String userId);
+    Optional<NotesEntity> findById(String id);
 }
