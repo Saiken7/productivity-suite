0b2159b95c85422d457ed5043ebe9191ed737a40
Kunal <saikenhere@gmail.com>
2025-11-23 12:00:49 +0530

    Added No Args Constructor

--- backend/LifeCanvas/src/main/java/com/productivity_suite/LifeCanvas/Requests/LoginRequestDTO.java
+++ backend/LifeCanvas/src/main/java/com/productivity_suite/LifeCanvas/Requests/LoginRequestDTO.java
@@ -6,10 +6,12 @@ import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class LoginRequestDTO {
     @NotBlank(message = "Email should not be Blank")
     @Email(message = "Enter Valid Email")
