22edf79a2082f219b949a3562a2b901ba0f7726b
Kunal <saikenhere@gmail.com>
2025-11-23 15:32:34 +0530

    Redis Service added to Goal Service

--- backend/LifeCanvas/src/main/java/com/productivity_suite/LifeCanvas/Services/GoalService.java
+++ backend/LifeCanvas/src/main/java/com/productivity_suite/LifeCanvas/Services/GoalService.java
@@ -1,12 +1,17 @@
 package com.productivity_suite.LifeCanvas.Services;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.productivity_suite.LifeCanvas.Entity.GoalsEntity;
+import com.productivity_suite.LifeCanvas.Entity.UserEntity;
 import com.productivity_suite.LifeCanvas.Repository.GoalsRepository;
+import com.productivity_suite.LifeCanvas.Repository.UserRepository;
 import com.productivity_suite.LifeCanvas.Responses.GoalsResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.parameters.P;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
@@ -18,22 +23,64 @@ public class GoalService {
     @Autowired
     private GoalsRepository goalsRepository;
 
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Autowired
+    private ObjectMapper mapper;
+
+
     // GET all Goals of a User
     public List<GoalsResponse> getUserGoals(String userId){
-        List<GoalsEntity> goal = goalsRepository.findByUserId(userId);
-        return goal.stream()
-                .map(this::convertToGoalResponse)
-                .toList();
+
+        Optional<UserEntity> exitsUser = userRepository.findByUserId(userId);
+
+        if(exitsUser.isEmpty()){
+            throw new RuntimeException("User Not Found");
+        }else{
+            String key = "Goals:"+userId;
+            Object cached = redisTemplate.opsForValue().get(key);
+
+            if(cached != null){
+                List<GoalsEntity> list = mapper.convertValue(cached, new TypeReference<List<GoalsEntity>>() {});
+                return list.stream()
+                        .map(this::convertToGoalResponse)
+                        .toList();
+            }
+
+            List<GoalsEntity> goal = goalsRepository.findByUserId(userId);
+            redisTemplate.opsForValue().set(key,goal, Duration.ofSeconds(600L));
+            return goal.stream()
+                    .map(this::convertToGoalResponse)
+                    .toList();
+        }
+
+
     }
 
     //GET selected goal of a user
     public GoalsResponse getGoal(String id){
         Optional<GoalsEntity> optional = goalsRepository.findById(id);
+
         if(optional.isEmpty()){
             throw new RuntimeException("Goal Not Found");
+        }else{
+            String key = "Goal:"+id;
+            Object cached = redisTemplate.opsForValue().get(key);
+
+            if(cached != null){
+                GoalsEntity response = mapper.convertValue(cached,GoalsEntity.class);
+                return convertToGoalResponse(response);
+            }
+
+            GoalsEntity goal = optional.get();
+            redisTemplate.opsForValue().set(key,goal, Duration.ofSeconds(600L));
+            return convertToGoalResponse(goal);
         }
-        GoalsEntity goal = optional.get();
-        return convertToGoalResponse(goal);
+
     }
 
 
@@ -94,6 +141,7 @@ public class GoalService {
     // CONVERTERS
     private GoalsResponse convertToGoalResponse(GoalsEntity goalsEntity){
         return GoalsResponse.builder()
+                .id(goalsEntity.getId())
                 .goalTitle(goalsEntity.getGoalTitle())
                 .startTime(goalsEntity.getGoalStartTime())
                 .endTime(goalsEntity.getGoalEndTime())
